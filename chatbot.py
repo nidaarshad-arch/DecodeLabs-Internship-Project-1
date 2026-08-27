8ba11e40a114e2e12f9fa836e3c7bb96f462c76b
Nida <nidaarshad309@gmail.com>
2026-08-27 22:10:55 +0500

    Handle casual conversation

--- chatbot.py
+++ chatbot.py
@@ -22,7 +22,7 @@ def run_chatbot():
         if not user_input:
             print(f"{BOT_name}: Please type something so I can help you.")
             continue
-        if user_input in {"exit","quit","bye","goodbye"}:
+        if user_input in {"exit","quit","bye","goodbye","ok bye","ok goodbye","okay bye","okay goodbye"}:
             print(f"{BOT_name}: Goodbye! Keep learning, keep building, and never stop exploring.")
             break
         if user_input in {"hello", "hi", "hey", "good morning", "good evening"}:
@@ -33,6 +33,39 @@ def run_chatbot():
                 "Hello! I'm happy to assist you."
                 ])
             print(f"{BOT_name}: {response}")
+
+        elif user_input in {"how are you", "how are you?", "how r you","how are u","how are u?","how r u","how r u?"}:
+            response = random.choice([
+                "I'm doing great! Thanks for asking. ",
+                "I'm functioning perfectly and ready to help!",
+                "I'm great! How about you?"
+                ])
+            print(f"{BOT_name}: {response}")
+        elif user_input in {"thank you", "thanks", "thankyou"}:
+            response = random.choice([
+                "You're welcome! 😊",
+                "Happy to help!",
+                "Anytime! Feel free to ask me more questions."
+                ])
+            print(f"{BOT_name}: {response}")
+        elif user_input in {"what is your name", "who are you", "tell me your name","Tell me about yourself","tell me about you"}:
+            print(
+                f"{BOT_name}: My name is {BOT_name}! "
+                "I'm a rule-based AI chatbot built using Python."
+                )
+        elif user_input in {"i am fine", "i'm fine", "im fine", "fine","I'm great","good","i am good","im good","great"}:
+            print(f"{BOT_NAME}: That's great to hear! 😊")
+        elif user_input == "help":
+            print(f"""{BOT_name}: Here are some things you can ask me
+            • Greetings: hello, hi, hey
+            • Small talk: how are you
+            • About me: who are you, what is your name
+            • AI topics: what is AI, machine learning
+            • Python topics: what is Python
+            • Fun: tell me a joke
+            • Time and date: what time is it, what is the date
+            • Exit: exit, quit, bye
+            """)
         
 
 if __name__ == "__main__":
