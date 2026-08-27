2a16c4ed9e9a975f2607dd1eca1aac5687949424
Nida <nidaarshad309@gmail.com>
2026-08-27 21:27:19 +0500

    Add display_welcome()

---
+++ chatbot.py
@@ -0,0 +1,19 @@
+import random
+from datetime import datetime
+
+BOT_name='ByteBot'
+
+def display_welcome():
+    print("="*65)
+    print(f"{BOT_name} - Rule-Based AI Chatbot")
+    print("="*65)
+    print(f"\n{BOT_name}:Hello! I am {BOT_name}","your rule-based AI assistant.")
+    print("You can ask me about AI, Machine Learning, Python, or simply have a small" \
+    "conversation with me.")
+    print("\n Type 'help to see available topics.")
+    print("Type 'exit' anytime to end the conversation.\n")
+
+
+
+
+
